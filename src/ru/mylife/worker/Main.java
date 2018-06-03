63c0ac799f772630ba6b98b8a942c3005fd0f8ec
AlekseyMishanin <mishanin_a_a@mail.ru>
2018-06-03 15:21:59 +0300

    Добавил if в цикл for each

--- src/ru/mylife/worker/Main.java
+++ src/ru/mylife/worker/Main.java
@@ -2,6 +2,14 @@ package ru.mylife.worker;
 
 import java.util.Random;
 
+/*
+ * Создать класс "Сотрудник" с полями: ФИО, должность, email, телефон, зарплата, возраст;
+ * Конструктор класса должен заполнять эти поля при создании объекта;
+ * Внутри класса «Сотрудник» написать метод, который выводит информацию об объекте в консоль;
+ * Создать массив из 5 сотрудников
+ * С помощью цикла вывести информацию только о сотрудниках старше 40 лет;
+ */
+
 public class Main {
 
     public static void main(String[] args) {
@@ -11,7 +19,7 @@ public class Main {
         String[] email = {"worker1@mail.ru","worker2@mail.ru","worker3@mail.ru","worker4@mail.ru","worker5@mail.ru"};
         String[] phone = {"89101001010","89102001010","89103001010","89104001010","89105001010"};
         int[] salary = {10000,15000,10000,20000,15000};
-        int[] age = {20,30,50,25,35};
+        int[] age = {20,30,50,45,35};
         Random rand = new Random();
         Worker[] worker = new Worker[5];
         for (int i = 0; i < worker.length; i++) {
@@ -23,7 +31,9 @@ public class Main {
                     age[rand.nextInt(5)]);
         }
         for (Worker t: worker) {
-            t.printWorker();
+            if(t.getAge()>40){
+                t.printWorker();
+            }
         }
     }
 }
